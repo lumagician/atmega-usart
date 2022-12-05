3898a9ae26bc63e12f598bf2f48c20bb3de42e3b
Leo Hofer <33592977+lumagician@users.noreply.github.com>
2022-12-05 23:18:17 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,13 @@
 
 #include <uart.h>
 
+// global interrupt flag
+volatile uint8_t interruptFlag = 0;
+
 int main(void)
 {
-    // initialize the UART
-    initUART(103);
+    // initialize the UART using the baud to UBRR conversion macro
+    initUART(UBRR_VALUE(9600));
     // initialize the global interrupts
     initGlobalInterrupts();
 
@@ -27,8 +30,20 @@ int main(void)
             // toggle PB5
             PORTB ^= (1 << PB5);
             // clear the interrupt flag
-            UCSR0A &= ~(1 << RXC0);
+            interruptFlag = 0;
         }
 
+    }
+}
+
+// interrupt handler for the UART receive interrupt
+ISR(USART_RX_vect)
+{
+    // read the received byte
+    uint8_t data = UDR0;
+    // check if the received byte is 'a'
+    if (data == 'a') {
+        // set interrupt flag
+        interruptFlag = 1;
     }
 }
